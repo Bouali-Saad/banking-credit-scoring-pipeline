3972179c8cd3f866577588f6d4de7153da3617e4
Bouali-Saad <bouali2019@icloud.com>
2026-04-15 15:58:01 +0100

    feat: add groupements metier + recence features + fix dates futures + suppression redondances

--- dbt/pfe_scoring/models/marts/dataset_ml_v3.sql
+++ dbt/pfe_scoring/models/marts/dataset_ml_v3.sql
@@ -18,7 +18,7 @@ SELECT
     f.periode_trt,
     f.is_prediction_period,
     f.flag_transfo,
-    f.date_trt_extr                             AS date_trt_extr_global,
+    f.date_trt_extr                                     AS date_trt_extr_global,
 
     
     s.age,
@@ -27,19 +27,73 @@ SELECT
     s.charges,
     s.mensualite_loyer,
     s.flag_eligible_md,
-    s.csp_mkt,
-    s.secteur_activite,
-    s.civilite_client,
-    s.prem_produit,
-    s.canal_ent_relation,
-    s.type_client,
-    s.activite_profession,
-    s.dernier_evt,
     s.anciennete_annees,
     s.anciennete_emploi,
     s.nb_jours_dernier_evt,
 
+   
+    s.csp_mkt,           
+    s.type_client,       
+   
+    CASE
+        WHEN s.civilite_client = 'Monsieur'                     THEN 'HOMME'
+        WHEN s.civilite_client IN ('Madame', 'Mademoiselle')    THEN 'FEMME'
+        WHEN s.civilite_client IS NULL                          THEN 'INCONNU'
+        ELSE                                                         'PERSONNE_MORALE'
+    END                                                         AS groupe_civilite,
+
     
+    CASE
+        WHEN s.prem_produit = 'Credit des menages'              THEN 'CREDIT_MENAGES'
+        WHEN s.prem_produit = 'Credit Auto'                     THEN 'CREDIT_AUTO'
+        WHEN s.prem_produit IN ('Toutes affaires LOA',
+                                'Affaire LOA')                  THEN 'LOA'
+        WHEN s.prem_produit = 'Credit Revolving'                THEN 'REVOLVING'
+        WHEN s.prem_produit IS NULL                             THEN 'INCONNU'
+        ELSE                                                         'AUTRES_PRODUITS'
+    END                                                         AS groupe_produit,
+
+   
+    CASE
+        WHEN s.dernier_evt = 'Demande de credit'                THEN 'DEMANDE_CREDIT'
+        WHEN s.dernier_evt = 'Demande de reclamation'           THEN 'RECLAMATION'
+        WHEN s.dernier_evt IS NULL                              THEN 'INCONNU'
+        ELSE                                                         'AUTRE_EVT'
+    END                                                         AS groupe_dernier_evt,
+
+    
+    CASE
+        WHEN a.canal_prov_principal IN ('MAILING','TELEMARKET','AFFICHAGE',
+                                        'DEPLIANT','BOUCHE A O','ENTREPRISE',
+                                        'SALON')                THEN 'CANAL_ACTIF'
+        WHEN a.canal_prov_principal IN ('ARCW','ARCD')          THEN 'CANAL_ARC'
+        WHEN a.canal_prov_principal IN ('PARRAINNAE','PRON_PAS','TV','NO VERT',
+                                        'INTERNET','PRESSE','1118','RADIO',
+                                        '1103','CRC','SMS')     THEN 'CANAL_INACTIF'
+        WHEN a.canal_prov_principal IS NULL                     THEN 'INCONNU'
+        ELSE                                                         'AUTRES'
+    END                                                         AS groupe_canal_prov,
+
+    
+    CASE
+        WHEN a.code_reseau_principal IN ('BMW','AUDI','MERC','ALFA','JAGU',
+                                         'LAND','VOLV','ROVE','CADI')
+                                                                THEN 'PREMIUM'
+        WHEN a.code_reseau_principal IN ('RENA','PEUG','CITR','FIAT','HYUN',
+                                         'TOYO','DACI','OPEL','SEAT','SKOD',
+                                         'FORD','VOLK')         THEN 'POPULAIRE'
+        WHEN a.code_reseau_principal IN ('KIA','MITS','NISS','HOND','SUZU',
+                                         'MAZD','DAEW','DAIH','SSAN')
+                                                                THEN 'ASIATIQUE'
+        WHEN a.code_reseau_principal IN ('MARJ','AUT2','DIGI','AEDM','E130',
+                                         'E250','E300','E400','E450','E500',
+                                         'E600','E700','E730','E800','E820',
+                                         'E960')                THEN 'RESEAU_BANQUE'
+        WHEN a.code_reseau_principal IS NULL                    THEN 'INCONNU'
+        ELSE                                                         'AUTRES'
+    END                                                         AS groupe_reseau,
+
+   
     a.nb_credits,
     a.moy_mt_init_brut,
     a.moy_mt_cap_rest,
@@ -67,10 +121,7 @@ SELECT
     a.flag_prel_prelevement,
     a.nb_credits_ctx,
     a.moy_retard_echeance,
-    a.moy_delai_extraction                      AS aff_moy_delai_extraction,
-    a.canal_prov_principal,
-    a.code_reseau_principal,
-    a.type_bien_principal,
+    a.moy_delai_extraction                                      AS aff_moy_delai_extraction,
 
     
     c.nb_campagnes,
@@ -83,76 +134,98 @@ SELECT
     c.nb_voice_failed,
     c.flag_canal_voice,
 
-    
+   
     sv.nb_sav,
-    sv.nb_agences                               AS sav_nb_agences,
-    sv.nb_affaires                              AS sav_nb_affaires,
+    sv.nb_agences                                               AS sav_nb_agences,
+    sv.nb_affaires                                              AS sav_nb_affaires,
     sv.flag_recouvrement,
     sv.flag_main_levee,
     sv.flag_modification,
     sv.flag_fidelisation,
     sv.flag_opposition,
-    sv.flag_cloture                             AS sav_flag_cloture,
+    sv.flag_cloture                                             AS sav_flag_cloture,
     sv.flag_changement_banque,
-    sv.flag_situation_credit                    AS sav_flag_situation_credit,
+    sv.flag_situation_credit                                    AS sav_flag_situation_credit,
     sv.flag_main_levee_auto,
     sv.flag_attestation_fin,
-    sv.flag_report_echeance                     AS sav_flag_report_echeance,
+    sv.flag_report_echeance                                     AS sav_flag_report_echeance,
     sv.flag_sav_actif,
     sv.flag_sav_cloture,
     sv.flag_sav_annule,
-    sv.flag_canal_tel                           AS sav_flag_canal_tel,
+    sv.flag_canal_tel                                           AS sav_flag_canal_tel,
     sv.nb_sav_non_clotures,
     sv.nb_sav_sans_delai,
-    sv.moy_duree_traitement                     AS sav_moy_duree_traitement,
-    sv.moy_delai_extraction                     AS sav_moy_delai_extraction,
-    sv.date_premier_sav,
-    sv.date_dernier_sav,
+    sv.moy_duree_traitement                                     AS sav_moy_duree_traitement,
+    sv.moy_delai_extraction                                     AS sav_moy_delai_extraction,
 
     
     r.nb_reclamations,
     r.flag_double_prelevement,
     r.nb_recla_non_clotures,
-    r.date_premiere_recla,
-    r.date_derniere_recla,
 
     
     d.nb_demandes,
     d.flag_demande_pret,
-    d.flag_sav                                  AS dem_flag_sav,
+    d.flag_sav                                                  AS dem_flag_sav,
     d.flag_simulation,
     d.flag_sort_dossier,
-    d.flag_report_echeance                      AS dem_flag_report_echeance,
+    d.flag_report_echeance                                      AS dem_flag_report_echeance,
     d.flag_rachat_credit,
-    d.moy_delai_extraction                      AS dem_moy_delai_extraction,
-    d.date_premiere_demande,
-    d.date_derniere_demande,
-
-   
+    d.moy_delai_extraction                                      AS dem_moy_delai_extraction,
 
    
     CASE
         WHEN s.revenu IS NULL OR s.revenu = 0 THEN NULL
         ELSE ROUND((a.moy_mensualite / s.revenu)::numeric, 4)
-    END                                         AS taux_endettement,
+    END                                                         AS taux_endettement,
 
-    -- 2. Score risque client (0=sain, 1=léger, 2=sérieux, 3=critique)
+    
     COALESCE(a.flag_impaye, 0)
     + COALESCE(sv.flag_recouvrement, 0)
-    + COALESCE(a.flag_contentieux, 0)           AS score_risque,
+    + COALESCE(a.flag_contentieux, 0)                          AS score_risque,
 
-   
+    
     CASE
         WHEN a.moy_mt_init_brut IS NULL OR a.moy_mt_init_brut = 0 THEN NULL
         ELSE ROUND((a.moy_mt_cap_rest / a.moy_mt_init_brut)::numeric, 4)
-    END                                         AS ratio_remboursement,
+    END                                                         AS ratio_remboursement,
 
-    -- 4. Flag nouveau client (ancienneté < 2 ans)
+   
     CASE
         WHEN s.anciennete_annees IS NULL THEN NULL
         WHEN s.anciennete_annees < 2     THEN 1
         ELSE 0
-    END                                         AS flag_nouveau_client
+    END                                                         AS flag_nouveau_client,
+
+    
+    CASE
+        WHEN sv.date_dernier_sav IS NULL THEN NULL
+        WHEN TO_TIMESTAMP(NULLIF(TRIM(sv.date_dernier_sav::text),''),
+                          'DDMONYYYY:HH24:MI:SS') > f.date_trt_extr THEN NULL
+        ELSE EXTRACT(DAY FROM f.date_trt_extr -
+                     TO_TIMESTAMP(NULLIF(TRIM(sv.date_dernier_sav::text),''),
+                                  'DDMONYYYY:HH24:MI:SS'))::numeric
+    END                                                         AS recence_sav,
+
+    
+    CASE
+        WHEN r.date_derniere_recla IS NULL THEN NULL
+        WHEN TO_TIMESTAMP(NULLIF(TRIM(r.date_derniere_recla::text),''),
+                          'DDMONYYYY:HH24:MI:SS') > f.date_trt_extr THEN NULL
+        ELSE EXTRACT(DAY FROM f.date_trt_extr -
+                     TO_TIMESTAMP(NULLIF(TRIM(r.date_derniere_recla::text),''),
+                                  'DDMONYYYY:HH24:MI:SS'))::numeric
+    END                                                         AS recence_recla,
+
+   
+    CASE
+        WHEN d.date_derniere_demande IS NULL THEN NULL
+        WHEN TO_TIMESTAMP(NULLIF(TRIM(d.date_derniere_demande::text),''),
+                          'DDMONYYYY:HH24:MI:SS') > f.date_trt_extr THEN NULL
+        ELSE EXTRACT(DAY FROM f.date_trt_extr -
+                     TO_TIMESTAMP(NULLIF(TRIM(d.date_derniere_demande::text),''),
+                                  'DDMONYYYY:HH24:MI:SS'))::numeric
+    END                                                         AS recence_demande
 
 FROM f
 
